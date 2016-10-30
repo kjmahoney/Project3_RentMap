45e011c0fbe740d2618af701c2d45d91d885a934
Khoi Le <khoileee89@gmail.com>
2016-10-30 19:13:19 -0400

    Region Scope seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,30 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Region.destroy_all
 
+
+# test41=Region.create(zip:"20009",state:"DC",city:"Washington",rent:"2750")
+# test42=Region.create(zip:"20002",state:"DC",city:"Washington",rent:"2599")
+# test164=Region.create(zip:"20011",state:"DC",city:"Washington",rent:"2526")
+# test233=Region.create(zip:"20019",state:"DC",city:"Washington",rent:"1922")
+# test298=Region.create(zip:"20001",state:"DC",city:"Washington",rent:"2858")
+# test404=Region.create(zip:"20020",state:"DC",city:"Washington",rent:"1975")
+# test1364=Region.create(zip:"20008",state:"DC",city:"Washington",rent:"3256")
+# test1733=Region.create(zip:"20032",state:"DC",city:"Washington",rent:"1913")
+# test2070=Region.create(zip:"20016",state:"DC",city:"Washington",rent:"3785")
+# test2256=Region.create(zip:"20003",state:"DC",city:"Washington",rent:"2850")
+# test2669=Region.create(zip:"20010",state:"DC",city:"Washington",rent:"2887")
+# test2701=Region.create(zip:"20007",state:"DC",city:"Washington",rent:"3896")
+# test5377=Region.create(zip:"20005",state:"DC",city:"Washington",rent:"2647")
+# test5557=Region.create(zip:"20017",state:"DC",city:"Washington",rent:"2479")
+# test5640=Region.create(zip:"20018",state:"DC",city:"Washington",rent:"2481")
+# test5661=Region.create(zip:"20024",state:"DC",city:"Washington",rent:"2195")
+# test6133=Region.create(zip:"20037",state:"DC",city:"Washington",rent:"2676")
+# test6712=Region.create(zip:"20015",state:"DC",city:"Washington",rent:"3976")
+# test7299=Region.create(zip:"20012",state:"DC",city:"Washington",rent:"2736")
+# test9551=Region.create(zip:"20036",state:"DC",city:"Washington",rent:"2262")
+# test12602=Region.create(zip:"20004",state:"DC",city:"Washington",rent:"2403")
+
+
 test1=Region.create(zip:"10025",state:"NY",city:"New York",rent:"3612")
 test2=Region.create(zip:"60657",state:"IL",city:"Chicago",rent:"1958")
 test3=Region.create(zip:"10023",state:"NY",city:"New York",rent:"3966")
