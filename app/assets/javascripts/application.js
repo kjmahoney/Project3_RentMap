a52d8a167e7cd18b5ba4bd4052b63f19fac8198a
Khoi Le <khoileee89@gmail.com>
2016-11-01 15:25:35 -0400

    initial googlePlaces functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 
 var map;
 var regions;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
@@ -24,7 +25,8 @@ function initMap() {
     center: {lat: 38.9072, lng: -77.0369},
     mapTypeId: 'roadmap'
   });
-
+  initAutocomplete();
+console.log("initMap is firing")
   $.ajax({
     url: "http://localhost:3000/regions",
     type: "GET",
@@ -33,8 +35,78 @@ function initMap() {
     regions = result
     console.log(regions)
     eqfeed_callback(regions)
+
   });
+
+  function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 38.9072, lng: -77.0369},
+      zoom: 12,
+      minZoom: 5,
+      mapTypeId: 'roadmap'
+    });
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    console.log(input)
+    var searchBox = new google.maps.places.SearchBox(input);
+    console.log("initAutocomplete")
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
 }
+
+
 
 function eqfeed_callback(regions) {
   var heatmapData = [];
@@ -43,11 +115,12 @@ function eqfeed_callback(regions) {
     var rentMagnitude = (regions[i].rent)
     heatmapData.push({location: latLng, weight: rentMagnitude});
   }
-
+console.log("heatmap")
   var heatmap = new google.maps.visualization.HeatmapLayer({
     data: heatmapData,
     dissipating: true,
-    radius: (map.zoom * 30),
+    radius: 60,
+    maxIntensity: 100000,
     map: map
   });
   // google.maps.event.addListener(map, 'zoom_changed', function() {
@@ -60,3 +133,11 @@ function eqfeed_callback(regions) {
   //   console.log(`radius is ${heatmap.radius}`)
   // })
 }
+
+// function initialize(){
+// initMap();
+// initAutocomplete();
+//
+//
+//   console.log("initialize is firing")
+// }
